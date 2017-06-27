1d486e45bc9fda5092f7f17cfca99c9452bd3153
tonymichaelhead <tonymichaelhead@gmail.com>
2017-06-27 09:33:03 -0700

    Finish getElementsByClassName.js and fix getElementsByClassNameSpec.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var newArray = [];
+  var element = document.body;
+  checkElement(element);
+  function checkElement(node) {
+    if (node.classList.contains(className)) {
+        newArray.push(node);
+    }
+    if (node.childNodes) {
+      for(var i=0; i < node.childNodes.length; i++) {
+        if (node.childNodes[i].nodeType !== Node.TEXT_NODE) {
+          checkElement(node.childNodes[i]);
+        }
+      }
+    }
+  }
+  return newArray;
 };
